65fec31b7f6a719462d5f1a41b3df3368c4f12af
iamdumonde <martinaboumonde@gmail.com>
2024-06-14 09:58:38 +0100

    sanctum installé & configuré

--- config/sanctum.php
+++ config/sanctum.php
@@ -16,9 +16,12 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
+        '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        // Sanctum::currentApplicationUrlWithPort(),
+        // env('FRONTEND_URL')
+        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : '',
+        env('FRONTEND_URL') ? ',' . parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
